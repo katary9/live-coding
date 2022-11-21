6807c327d6e3abd06bcabefa4fab5eb3859ce669
Kateryna Drozdova <katary379@gmail.com>
2022-11-21 12:46:51 +0200

    fix func

--- task-dom5/index.js
+++ task-dom5/index.js
@@ -1,7 +1,6 @@
 export function setTitle(text) {
     const titleEl = document.querySelector('.title');
-    titleEl.textContent = text;
-    return titleEl.textContent;
+   titleEl.textContent = text;
+   return titleEl.textContent;
 }
 
-setTitle('text');

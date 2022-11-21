a76c7986d25498b252094d7b833cc2908bd4b9e8
Kateryna Drozdova <katary379@gmail.com>
2022-11-21 13:06:10 +0200

    fixed

--- task-dom6/index.js
+++ task-dom6/index.js
@@ -1,7 +1,7 @@
 export function setButton (buttonText) {
     const bodyEl = document.querySelector('body');
-    // bodyEl.innerHTML = buttonText;
-    console.dir(bodyEl.innerHTML);
+    bodyEl.innerHTML = buttonText;
+    // console.dir(bodyEl.innerHTML);
     bodyEl.textContent = buttonText;
     // console.dir(bodyEl.textContent);
 

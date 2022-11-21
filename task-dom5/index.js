ce100106951b9514b32d3e387be52842944df40f
Kateryna Drozdova <katary379@gmail.com>
2022-11-21 12:39:30 +0200

    fixed

--- task-dom5/index.js
+++ task-dom5/index.js
@@ -1,6 +1,7 @@
 export function setTitle(text) {
     const titleEl = document.querySelector('.title');
     titleEl.textContent = text;
+    return titleEl.textContent;
 }
 
-setTitle('');
+setTitle();

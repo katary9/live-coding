6790e2699badaf6fd5fb404e190e1a14369468c4
Kateryna Drozdova <katary379@gmail.com>
2022-11-24 14:41:36 +0200

    fix

--- task-event1/index.js
+++ task-event1/index.js
@@ -19,16 +19,6 @@ const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-divElem.addEventListener('click', logGreyDiv, true);
-divElem.addEventListener('click', logGreenDiv);
-
-pElem.addEventListener('click', logGreyP, true);
-pElem.addEventListener('click', logGreenP);
-
-
-spanElem.addEventListener('click', logGreySpan, true);
-spanElem.addEventListener('click', logGreenSpan);
-
 function clear() {
   const eventEl = document.querySelector('.events-list');
   eventEl.innerHTML = '';
@@ -57,7 +47,7 @@ function remove() {
   spanElem.removeEventListener('click', logGreySpan, true);
   spanElem.removeEventListener('click', logGreenSpan);
 }
-remove();
+
 removeBtn.addEventListener('click', remove);
 
 
